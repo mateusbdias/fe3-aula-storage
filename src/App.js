ab83d6b8b4921bfd02d94f7a4cb9a7745f0b5500
Mateus Benites Dias <imattdias@gmail.com>
2022-04-19 15:26:33 -0300

    CTD - Front End III - Aula localStorage - Exercício Sync

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState("");
+
+  function addTask() {
+    setTasks([...tasks, input]);
+
+    localStorage.setItem('@todo_tasks', JSON.stringify([...tasks, input]));
+  }
+
+  useEffect(() => {
+    const tasksStorage = localStorage.getItem('@todo_tasks');
+
+    if(tasksStorage) {
+      const tempTasks = JSON.parse(tasksStorage);
+      setTasks(tempTasks);
+    }
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World</h1>
+      <input onChange={(event) => setInput(event.target.value)} />
+      <button onClick={addTask}>Salvar</button>
+      {tasks.map((task) => (
+        <p>{task}</p>
+      ))}
     </div>
   );
 }
